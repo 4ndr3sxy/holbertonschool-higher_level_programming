6f3eb8e4aa6ad00696db6ccd82cf263fb38e9dcb
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-09-06 18:51:49 -0500

    [FIX] - Fix efficiency

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,7 +5,10 @@ int check_cycle(listint_t *list)
 	listint_t *temp1 = list;
 	listint_t *temp2 = list;
 
-	while (temp1->next && temp2)
+	if (!list)
+		return (0);
+
+	while (temp2 && temp2->next)
 	{
 		temp1 = temp1->next;
 		temp2 = temp2->next->next;
@@ -13,7 +16,6 @@ int check_cycle(listint_t *list)
 		if (temp1 == temp2)
 		{
 			return (1);
-			break;
 		}
 	}
 	return (0);
