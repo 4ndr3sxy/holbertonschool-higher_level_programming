3d385f4233ee7631d3c32925da0f3846fd000c3a
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-09-06 18:38:59 -0500

    [ADD] - Creation code and file task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp1 = list;
+	listint_t *temp2 = list;
+
+	while (temp1 && temp2)
+	{
+		temp1 = temp1->next;
+		temp2 = temp2->next->next;
+
+		if (temp1 == temp2)
+		{
+			return (1);
+			break;
+		}
+	}
+	return (0);
+}
