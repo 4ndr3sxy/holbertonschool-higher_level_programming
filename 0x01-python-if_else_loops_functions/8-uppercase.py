25f9ebc9292df6ebe79b51f04ce3c0f1a1f1520b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-07 15:13:35 -0500

    [FIX] - Fix code in .format()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 def uppercase(str):
     for char in str:
         asC = ord(char)
-        print(chr(asC-32) if asC >= 97 and asC <= 122 else char, end='')
+        print("{:c}\
+".format(asC-32 if asC >= 97 and asC <= 122 else asC), end='')
     print()
