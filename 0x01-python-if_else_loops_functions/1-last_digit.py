55c8291055961882fbfc8dfb989b20b4df773f06
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-07 14:24:18 -0500

    [FIX] - Fix logix module negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 flag = 0
 if number < 0:
-    lastNumber = (number * -1) % 10
-    flag = 1
+    module = -10
 else:
-    lastNumber = number % 10
-
+    module = 10
+lastNumber = number % module
 if lastNumber > 5:
     print("Last digit of {:d} is {:d} \
 and is greater than 5".format(number, lastNumber))
@@ -15,9 +14,5 @@ elif lastNumber == 0:
     print("Last digit of {:d} is {:d} and is 0\
     ".format(number, lastNumber))
 else:
-    if flag == 1:
-        print("Last digit of {:d} is {:d} and is less \
-than 6 and not 0".format(number, (lastNumber * -1)))
-    else:
-        print("Last digit of {:d} is {:d} and is less \
+    print("Last digit of {:d} is {:d} and is less \
 than 6 and not 0".format(number, lastNumber))
