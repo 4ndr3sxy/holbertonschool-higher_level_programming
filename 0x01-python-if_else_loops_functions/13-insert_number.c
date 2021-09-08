d0cf0c5ae44da01354cdd2b9ce4de0217283e6e6
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-07 20:04:38 -0500

    [FIX] - Fix code in asignation of nodes

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,7 +13,7 @@ listint_t *insert_node(listint_t **head, int number)
 	newNode->n = number;
 	if (!nodeMin || number <= (*head)->n)
 	{
-		newNode->next = *head;
+		newNode->next = nodeMin;
 		*head = newNode;
 		return (newNode);
 	}
