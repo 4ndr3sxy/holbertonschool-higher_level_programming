990631bdfb9b269bd26f50b1fcde264a6ab2a890
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-08 18:43:57 -0500

    [FIX] - Fix code in while conditional

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -5,40 +5,35 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *newNode = NULL;
 	listint_t *nodeMin = *head;
-	listint_t *nodeMax = (*head)->next;
 
 	newNode = malloc(sizeof(listint_t));
 	if (!newNode)
 		return (NULL);
 	newNode->n = number;
-	if (!nodeMin || number <= (*head)->n)
+	if (nodeMin == NULL || nodeMin->n >= number)
 	{
 		newNode->next = nodeMin;
 		*head = newNode;
 		return (newNode);
 	}
-	if (!nodeMax && nodeMin)
+	if (nodeMin && !nodeMin->next)
 	{
 		nodeMin->next = newNode;
 		newNode->next = NULL;
 		return (newNode);
 	}
-	while (nodeMin && nodeMax->next)
+	if (!nodeMin->next->next)
 	{
-		if (nodeMin->n <= number && nodeMax->n >= number)
-		{
-			newNode->next = nodeMax;
-			nodeMin->next = newNode;
-			return (newNode);
-		}
-		nodeMin = nodeMin->next;
-		nodeMax = nodeMax->next;
-	}
-	if (!nodeMax->next)
-	{
-		nodeMax->next = newNode;
+		nodeMin->next->next = newNode;
 		newNode->next = NULL;
 		return (newNode);
 	}
+	while (nodeMin && nodeMin->next && number > nodeMin->next->n)
+	{
+		nodeMin = nodeMin->next;
+	}
+
+	newNode->next = nodeMin->next;
+	nodeMin->next = newNode;
 	return (newNode);
 }
