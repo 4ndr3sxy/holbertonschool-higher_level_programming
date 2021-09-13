7d881bd90b91ef844209e088057ace8776aa535c
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-09-13 14:27:19 -0500

    [ADD] - Creation code and file to task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    size = len(my_string)
+    cs = 0
+    for c in my_string:
+        if c == "c" or c == "C":
+            cs += 1
+    for i in range(0, size - cs):
+        if not my_string[i]:
+            break
+        if my_string[i] == "c" or my_string[i] == "C":
+            my_string = my_string[:i] + my_string[i + 1:]
+    return my_string
