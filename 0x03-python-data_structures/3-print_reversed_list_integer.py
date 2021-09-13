2a672eab56292c1d98729904cd5564ef138c2069
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-09-13 13:56:22 -0500

    [ADD] - Edge case exist list added

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     size = len(my_list)
-    if size == 0:
+    if not my_list:
         return
     for i in range(size - 1, -1, -1):
         number = my_list[i]
