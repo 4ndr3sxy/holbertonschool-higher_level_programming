68813be35f90519a167b5c95a007084eb6c1d636
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-09-13 18:43:08 -0500

    [FIX] - Fix logic code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    size_one = len(tuple_a)
-    size_two = len(tuple_b)
-    one = one_t = two = two_t = 0
-    if size_one == 1:
-        one = tuple_a[0]
-    if size_two == 1:
-        two = tuple_b[0]
-    if size_one == 2:
-        one = tuple_a[0]
-        one_t = tuple_a[1]
-    if size_two == 2:
-        two = tuple_b[0]
-        two_t = tuple_b[1]
-
-    add_one = one + two
-    add_two = one_t + two_t
-    return add_one, add_two
+    if len(tuple_a) == 0:
+        tuple_a = 0, 0
+    if len(tuple_b) == 0:
+        tuple_b = 0, 0
+    if len(tuple_a) == 1:
+        tuple_a = tuple_a[0], 0
+    if len(tuple_b) == 1:
+        tuple_b = tuple_b[0], 0
+    tuple_c = tuple_a[0] + tuple_b[0] , tuple_a[1] + tuple_b[1]
+    return tuple_c
