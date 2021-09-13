feb1e6c025f7ccc321691bb69a202f5e3dc8410b
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-09-13 18:20:14 -0500

    [FIX] - Fix logic code

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if not my_list:
-        return my_list
     size = len(my_list)
-    if size < idx or idx < 0:
-        return my_list
-    del my_list[idx]
+    if idx >= 0 and size > idx:
+        del my_list[idx]
     return my_list
