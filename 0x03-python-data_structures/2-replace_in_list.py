238b43b4134196c67be297a02a2ec057d4d0620e
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-09-13 13:39:34 -0500

    [FIX] - Fix code in L.4, countValues - 1

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     countValues = len(my_list)
-    if idx < 0 or idx > countValues:
+    if idx < 0 or idx > countValues - 1:
         return my_list
     my_list[idx] = element
     return my_list
