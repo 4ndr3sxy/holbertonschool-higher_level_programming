2aaebd0ff78aca477de356534fb3892f98aa855a
Andres Castaneda <andrescastanedaxy@gmail.com>
2021-09-13 13:38:03 -0500

    [FIX] - Fix code in condition with countValues

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     countValues = len(my_list)
-    if idx < 0 or idx > countValues:
+    if idx < 0 or idx > countValues - 1:
         return "None"
     valueIndex = my_list[idx]
     return valueIndex
