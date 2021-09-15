8ddcfdb75c0761a0f715b501ffa6ae3c84d18234
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-14 19:18:04 -0500

    [FIX] - Fix code, edge case one key added

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     validator = 1
-    while validator == 1:
+    while validator == 1 and a_dictionary:
         for keys, values in a_dictionary.items():
             if values == value:
                 validator = 1
