d3f736e750fca327a714ffb54243ab73d0a7bdf5
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-27 15:29:15 -0500

    [FIX] - Fix code in positions of return

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,10 +4,9 @@ stderr_fileno = sys.stderr
 
 
 def safe_function(fct, *args):
-    validate = True
     try:
         result = fct(args[0], args[1])
+        return True
     except Exception as ex:
         stderr_fileno.write("Exception: {}\n".format(ex))
-        validate = False
-    return result if validate else None
+        return False
