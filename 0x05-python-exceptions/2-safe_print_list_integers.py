a575e0af8cd0a70d2b962c804c3f52af3676ddd9
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-27 14:23:27 -0500

    [FIX] - Fix code, in logic for loop

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for val in my_list:
+    for i in range(0, x):
         try:
-            if count >= x:
-                break
-            print("{:d}".format(val), end='')
+            print("{:d}".format(my_list[i]), end='')
             count += 1
         except (TypeError, ValueError):
             pass
