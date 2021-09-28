5bfb021a8a2e9f8260c8dfbc5190be33a866de72
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-28 15:30:43 -0500

    [ADD] - Creation code and file task 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    dict
+
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+            if size < 0:
+                raise Exception("size must be >= 0")
+        except TypeError:
+            raise Exception("size must be an integer")
