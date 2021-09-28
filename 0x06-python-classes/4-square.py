82db3c24c0e1fa0a33259131292b331cf77788e5
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-28 16:08:36 -0500

    [FIX] - Fix doc

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
+"""Class/module Square"""
+
+
 class Square:
+    """Object Square"""
     dict
 
     def __init__(self, size=0):
-        try:
-            self.__size = size
-            if size < 0:
-                raise Exception("size must be >= 0")
-        except TypeError:
-            raise Exception("size must be an integer")
+        """Contructor"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
+    """Area of square"""
     def area(self):
         return self.__size * self.__size
 
+    """Get size"""
     def get_size(self):
         return self.__size
 
+    """Set size"""
     def set_size(self, value):
         self.__init__(value)
 
