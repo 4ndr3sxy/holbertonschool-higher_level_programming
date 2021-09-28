0aff62e03fc8c7ac1db12f87a1b93b6542c124fd
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-28 15:56:27 -0500

    [FIX] - Fix style pep8 and documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Class/module Square"""
+
+
 class Square:
+    """Object Square"""
     dict
 
     def __init__(self, size):
+        """Contructor"""
         self.__size = size
