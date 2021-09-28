28882367a9a2e6d79a9d32bc93317405fe0a47a4
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-28 16:20:01 -0500

    [FIX] - Fix code in Style and Doc

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,43 +1,46 @@
 #!/usr/bin/python3
+"""Class/module Square"""
+
+
 class Square:
+    """Object Square"""
     dict
 
-    def __init__(self, size=0, position=(0, 0)):
-        try:
-            self.__size = size
-            self.__position = position
-            if size < 0:
-                raise Exception("size must be >= 0")
-            if position[0] < 0 or position[1] < 0:
-                raise TypeError("position must be a tuple\
- of 2 positive integers")
-        except TypeError:
-            raise Exception("size must be an integer")
+    def __init__(self, size=0):
+        """Contructor"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
+    """Area of square"""
     def area(self):
         return self.__size * self.__size
 
+    """Get size"""
+    def get_size(self):
+        return self.__size
+
+    """Set size"""
+    def set_size(self, value):
+        self.__init__(value)
+
+    """Print square"""
     def my_print(self):
         if self.__size == 0:
             print()
         else:
             for i in range(0, self.__size):
-                for j in range(0, self.__size + self.__position[0]):
-                    if j < self.__position[0]:
-                        print(" ", end='')
-                    else:
-                        print("#", end='')
+                for j in range(0, self.__size):
+                    print("#", end='')
                 print()
 
-    def get_size(self):
-        return self.__size
-
-    def set_size(self, value):
-        self.__init__(value)
-
+    """Get position"""
     def get_position(self):
         return self.__position
 
+    """Set position"""
     def set_position(self, value):
         self.__init__(self.__size, value)
 
