73a921b002047a005ddb6e146862838497ac6777
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-29 16:52:44 -0500

    [ADD] - Conditional to validation position[1] added

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,8 +26,9 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for h in range(0, self.__position[1]):
-                print()
+            if self.__position[1] > 0:
+                for h in range(0, self.__position[1]):
+                    print()
             for i in range(0, self.__size):
                 for j in range(0, self.__size + self.__position[0]):
                     if j < self.__position[0]:
