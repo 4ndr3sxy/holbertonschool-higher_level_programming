cd15e1913f59afe81f2d7cfa5f3c1eab2ba88ede
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-09-30 18:53:25 -0500

    [FIX] - Fix return and name of variable

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(string=[]):
-    string += ["BestSchool"]
-    return ", ".join(string)
+def magic_string(val=[]):
+    val += ["BestSchool"]
+    return (", ".join(val))
