efa9b2c44fc6bd04802a2e21bf3fe6b9940ec8c8
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-06 11:47:39 -0500

    [ADD] - style doc added to task 7

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Function lazy_matrix_mul multiply two matrix with numpy"""
+
+
 def lazy_matrix_mul(m_a, m_b):
+    """Args: m_a, m_b
+        return multiply of two matrix
+    """
     import numpy as np
     if len(m_a) == len(m_b):
         return np.matmul(m_a, m_b)
@@ -7,4 +13,3 @@ def lazy_matrix_mul(m_a, m_b):
         a = np.array(m_a)
         b = np.array(m_b)
         return np.dot(a, b)
-
