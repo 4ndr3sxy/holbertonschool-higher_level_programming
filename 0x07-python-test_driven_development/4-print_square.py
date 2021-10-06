026bca04a26c4fb585e8340b76065ccddba506d3
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-06 11:48:09 -0500

    [ADD] - style doc added to task 3

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Function print_square, print a square"""
+
+
 def print_square(size):
+    """Args: size
+        without return"""
     if type(size) is not int or (type(size) is float and size < 0):
         raise TypeError("size must be an integer")
     if size < 0:
