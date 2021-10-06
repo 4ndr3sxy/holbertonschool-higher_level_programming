7bdd3e5da18656ff05593cac2c308c080ee88f9c
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-06 11:47:59 -0500

    [ADD] - style doc added to task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Function say_my_name print fist and last name"""
+
+
 def say_my_name(first_name, last_name=""):
+    """Args: first_name, last_name
+        without return"""
     if type(first_name) is not str or type(last_name) is not str:
         raise TypeError("{} must be a string".format(
             "first_name" if type(first_name) is not str else "last_name"))
