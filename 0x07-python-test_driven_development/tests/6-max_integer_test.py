9c0f1022a16806d4bf94c02362c05b310e803a6e
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-08 20:46:04 -0500

    [ADD] - New test added

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -18,6 +18,10 @@ class TestMaxInteger(unittest.TestCase):
         array = [4.5, 4, 3, 2.8, 1]
         self.assertEqual(max_integer(array), 4.5)
 
+    def test_00_case_base_success_int_float_middle(self):
+        array = [4.5, 4, 88.3, 2.8, 1]
+        self.assertEqual(max_integer(array), 88.3)
+
     def test_00_case_base_success_one_value_float(self):
         array = [99.5]
         self.assertEqual(max_integer(array), 99.5)
