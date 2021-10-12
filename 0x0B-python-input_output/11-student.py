644e57ac4449037d11bfbfc1342e2b3c6bea18fa
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 17:58:49 -0500

    [FIX] - Fix Documentation and conditional to attrs

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+"""Class/object Student"""
+
+
 class Student:
+    """
+    Constructor
+        attributes are publics
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
+    """Return a dictionary representation with filter"""
     def to_json(self, attrs=None):
-        if not attrs:
+        if attrs is None:
             return self.__dict__
         new_dict = dict()
         for (key) in self.__dict__:
@@ -14,6 +22,7 @@ class Student:
                 new_dict[key] = self.__dict__[key]
         return new_dict
 
+    """Load new info to a object from a json file"""
     def reload_from_json(self, json):
         for key in json:
             for key_self in self.__dict__:
