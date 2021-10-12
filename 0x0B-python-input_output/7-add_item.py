e613f1218d44777cb1868c3238d13aab061f5c92
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 16:44:01 -0500

    [ADD] - Creation code and file task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+try:
+    file_name="add_item.json"
+    information = load_from_json_file(file_name)
+except:
+    information = []
+for obj in sys.argv[1:]:
+    information.append(obj)
+save_to_json_file(information, file_name)
+retrun 
