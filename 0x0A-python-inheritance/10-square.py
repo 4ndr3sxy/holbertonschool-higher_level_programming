61c1b6da2467db3b576445bb22e4008534a5b271
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-11 21:50:21 -0500

    [FIX] - Fix code in doc

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Import class to inheritance"""
-Rectangle_ = __import__('9-rectangle').Rectangle
 
-"""Class/object Square that inherits of Rectangle"""
+Rectangle_ = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle_):
