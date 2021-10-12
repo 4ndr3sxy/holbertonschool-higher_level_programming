f1abe381a31938080bae793cefffd814f646257a
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-11 20:30:45 -0500

    [FIX] - Fix style code doc

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 """Validate is instance obj of a_class"""
 
 
 def is_kind_of_class(obj, a_class):
+    """obj and a_class used"""
     if isinstance(obj, a_class):
         return True
     return False
