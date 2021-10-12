83f1ddfd554c922b8bda257b48c1680350a19b5d
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 17:38:41 -0500

    [FIX] - Documentation

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+
+"""Class/object Student"""
+
+
 class Student:
+    """
+    Constructor
+        attributes are publics
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
+    """Return a dictionary representation with filter"""
     def to_json(self, attrs=None):
         if not attrs:
             return self.__dict__
