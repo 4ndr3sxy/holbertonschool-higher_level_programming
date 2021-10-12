512ddb044b9c8c3b54826b563369c01bd1312419
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 16:59:26 -0500

    [FIX] - Fix code, style pep8 and doc

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,13 @@
+#!/usr/bin/python3
+"""Function to write in a file, with used"""
+
+
 def write_file(filename="", text=""):
+    """
+        args:
+            filename, name of file to write
+            text, text to write in the file
+    """
     with open(filename, 'w') as f:
         count_chars = f.write(text)
     return count_chars
