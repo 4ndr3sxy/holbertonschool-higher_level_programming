4c5f3c328a19e737bbea870fe6851fb59cabf848
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 17:31:49 -0500

    [FIX] - Documentation

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Function to load a simple data of the a class """
+
+
 def class_to_json(obj):
+    """
+        args:
+            obj, object type class
+    """
     obj_info = obj.__dict__
     return obj_info
