f9754a2b7f7cf74d615e45cb505f38d65f9e8eb8
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 17:12:14 -0500

    [FIX] - Fix code, style pep8 and doc

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Import json"""
 import json
+
+"""Function to Convert from Python to JSON and save in a file"""
+
+
 def save_to_json_file(my_obj, filename):
+    """
+        args:
+            my_obj, object to save
+            filename, name of file .json
+    """
     with open(filename, 'w+') as f:
-        read_data = f.read()
         f.write(json.dumps(my_obj))
     f.closed
