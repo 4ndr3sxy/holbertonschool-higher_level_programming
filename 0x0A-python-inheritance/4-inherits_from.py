073e7d3f30c29c2d7e20a978dc8ad222cdbaa7e5
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-11 20:32:28 -0500

    [FIX] - Fix code in style of doc

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Validate inherence"""
+
+
 def inherits_from(obj, a_class):
+    """obj and a_class used"""
     if type(obj) is a_class:
         return False
     if isinstance(obj, a_class):
