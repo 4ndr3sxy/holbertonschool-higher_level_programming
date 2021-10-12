48fb5b5e446aa415bd465012e0669ba9998d1aea
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 17:01:12 -0500

    [FIX] - Fix code, style pep8 and doc

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,13 @@
+#!/usr/bin/python3
+"""Function to write/append in a file, with used"""
+
+
 def append_write(filename="", text=""):
+    """
+        args:
+            filename, name of file to append text
+            text, text to append in the file
+    """
     with open(filename, 'a') as f:
         count_chars = f.write(text)
     return count_chars
