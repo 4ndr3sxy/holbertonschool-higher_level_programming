8bc247a4958e336c1bab8c826023d3850839d276
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 16:56:42 -0500

    [FIX] - Fix code, style pep8 and doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Function to read a file, with used"""
+
+
 def read_file(filename=""):
+    """
+        args: filename, name of file to read
+    """
     with open(filename) as f:
         read_data = f.read()
     print(read_data, end='')
