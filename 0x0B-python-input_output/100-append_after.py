520fadf163d85674f5f5c048e6a3fdd20f5ee6f2
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 16:45:17 -0500

    [ADD] - Creation code and file task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def append_after(filename="", search_string="", new_string=""):
+    inputfile = open(filename, 'r').readlines()
+    write_file = open(filename, 'w')
+    for line in inputfile:
+        write_file.write(line)
+        if search_string in line:
+            write_file.write(new_string)
+    write_file.close()
