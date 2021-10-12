fc30e5eecbb540edb4b9bc5b444614a8fa05cdcf
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 17:06:07 -0500

    [FIX] - Fix code, style pep8 and doc

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""Import json"""
 import json
 
+"""Function to Convert from Python to JSON"""
+
+
 def to_json_string(my_obj):
+    """
+        args:
+            my_obj, object to convert
+    """
     json_string = json.dumps(my_obj)
     return json_string
