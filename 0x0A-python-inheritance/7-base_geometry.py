1c6f9e61b8db050492fe320d86fe5453c387a8cb
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-11 20:38:21 -0500

    [FIX] - Fix code in statement of the class and pass deleted

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,15 +2,14 @@
 """Class/Object BaseGeometry"""
 
 
-class BaseGeometry():
-    pass
-
+class BaseGeometry:
     """classmethod with raise if area no implemented"""
     @classmethod
     def area(self):
         raise Exception("area() is not implemented")
 
-    """classmethod to validate value; if value not is an int or > 0, generate a raise"""
+    """classmethod to validate value;
+    if value not is an int or > 0, generate a raise"""
     @classmethod
     def integer_validator(self, name, value):
         if type(value) is not int:
