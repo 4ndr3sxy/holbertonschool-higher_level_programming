451fd1557c0a3d71f08feaea19dec3bb7d516fc2
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-11 21:43:51 -0500

    [ADD] - space in L.3

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Import class to inheritance"""
+
 BaseGeometry_ = __import__('7-base_geometry').BaseGeometry
 
 """Class/object REctangle that inherits of BaseGeometry"""
 
 
 class Rectangle(BaseGeometry_):
-
     """Constructor with inheritance"""
     def __init__(self, width, height):
         self.__width = BaseGeometry_.integer_validator("width", width)
