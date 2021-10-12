a4cf9195d93688b79737d4f02bcb606557e50e73
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-12 17:14:06 -0500

    [FIX] - Fix code, style pep8 and doc

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Import json"""
 import json
+
+"""Function to load a file .json"""
+
+
 def load_from_json_file(filename):
+    """
+        args:
+            filename, name of file .json
+    """
     with open(filename, 'r+') as f:
         read_data = f.read()
     new_json = json.loads(read_data)
