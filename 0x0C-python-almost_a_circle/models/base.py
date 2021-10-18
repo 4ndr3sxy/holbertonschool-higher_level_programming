a7df81d605ec4ad5c368480befab8f9578e3700b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-18 15:49:58 -0500

    [FIX] - Fix logic

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,10 +21,9 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Convert from python to Json"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
