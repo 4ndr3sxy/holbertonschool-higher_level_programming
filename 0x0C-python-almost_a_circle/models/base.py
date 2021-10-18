f075643eafc18c2855383313c674d7856bcbdb36
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-10-18 15:29:40 -0500

    [FIX] - Fix style pep8 and logic in static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,24 +10,25 @@ class Base:
     """No inheritance"""
     __nb_objects = 0
 
-    """Constructor"""
     def __init__(self, id=None):
+        """Constructor"""
         if id is not None:
             self.id = id
         else:
             self.__class__.__nb_objects += 1
             self.id = self.__class__.__nb_objects
 
-    """Convert from python to Json"""
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Convert from python to Json"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
-    """Create file .json and save objects"""
     @classmethod
     def save_to_file(cls, list_objs):
+        """Create file .json and save objects"""
         new_list = []
         filename = str(cls.__name__) + ".json"
         with open(filename, 'w') as f:
@@ -36,14 +37,14 @@ class Base:
                 new_list.append(obj_converted)
             f.write(cls.to_json_string(new_list))
 
-    """Convert from Json to Python object"""
     @staticmethod
     def from_json_string(json_string):
+        """Convert from Json to Python object"""
         return json.loads(json_string)
 
-    """Create instance using cls"""
     @classmethod
     def create(cls, **dictionary):
+        """Create instance using cls"""
         if cls.__name__ == "Square":
             new_obj = cls(1)
         else:
@@ -51,9 +52,9 @@ class Base:
         new_obj.update(**dictionary)
         return new_obj
 
-    """Load information of file .json"""
     @classmethod
     def load_from_file(cls):
+        """Load information of file .json"""
         new_list = []
         filename = str(cls.__name__) + ".json"
         with open(filename, 'r') as f:
