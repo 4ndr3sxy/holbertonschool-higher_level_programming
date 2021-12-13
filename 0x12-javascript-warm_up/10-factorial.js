1066db9490c49f10539074777031ff13bfa2a4d0
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-13 15:18:17 -0500

    [UPD] - order code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+const arg = process.argv.slice(2);
+console.log(factorialR(parseInt(arg[0])));
+
 function factorialR (num) {
     if (num === 1 || isNaN(num)) {
         return 1;
     } else {
-        return num * factorial_r(num - 1);
+        return num * factorialR(num - 1);
     }
 }
 
-const arg = process.argv.slice(2);
-console.log(factorialR(parseInt(arg[0])));
