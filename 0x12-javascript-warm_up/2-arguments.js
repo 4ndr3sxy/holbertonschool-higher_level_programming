bc0744e5b56de26d1925ab2158f27d0886225ac0
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-13 13:17:35 -0500

    [ADD] - Creation code and file task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const arg = process.argv.slice(2)
+const countArg = Object.keys(arg).length
+switch (countArg)
+{
+    case 0:
+        console.log('No argument')
+        break
+    case 1:
+        console.log('Argument found')
+        break
+    default:
+        console.log('Arguments found')
+        break
+}
