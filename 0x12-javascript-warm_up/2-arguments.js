3eb02eeaed5803ca1f2491d10d4278d78ee55eed
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-13 14:52:51 -0500

    [UPD] - tabs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,13 @@
 const arg = process.argv.slice(2);
 const countArg = Object.keys(arg).length;
 switch (countArg) {
-    case 0:
-        console.log('No argument');
-        break;
-    case 1:
-        console.log('Argument found');
-        break;
-    default:
-        console.log('Arguments found');
-        break;
+	case 0:
+		console.log('No argument');
+		break;
+	case 1:
+		console.log('Argument found');
+		break;
+	default:
+		console.log('Arguments found');
+		break;
 }
