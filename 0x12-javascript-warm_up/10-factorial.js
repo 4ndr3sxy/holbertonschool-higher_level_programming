87c115c789facbe00b7c5788ff783d9e72ea12b1
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-13 13:18:39 -0500

    [ADD] - Creation code and file task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function factorial_r(num)
+{
+    if (num === 1 || isNaN(num))
+    {
+        return 1
+    }
+    else
+    {
+        return num * factorial_r(num-1)
+    }
+}
+
+const arg = process.argv.slice(2)
+console.log(factorial_r(parseInt(arg[0])))
