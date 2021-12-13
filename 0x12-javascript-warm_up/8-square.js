1e0d081bac1dfc0721393637baabfeb830889067
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-13 13:18:13 -0500

    [ADD] - Creation code and file task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+function square(count, value)
+{
+    if (count === 0)
+    {
+        console.log(value)
+    }
+    else
+    {
+        count--
+        value += 'X'
+        square(count, value)
+    }
+}
+
+const arg = process.argv.slice(2)
+if (isNaN(arg[0]))
+    console.log('Missing size')
+else
+    for (let i = 0; i < arg[0]; i++) {
+        square(arg[0], "")
+    }
+
