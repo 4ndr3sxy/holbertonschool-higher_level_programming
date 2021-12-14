4bfffe059c5e88d41e50252dbf1eadc78ba5147a
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-14 12:36:09 -0500

    [ADD] - Creation code and file task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const SquareI = require('./5-square');
+class Square extends SquareI {
+  charPrint (c) {
+    let result = '';
+    if (c !== undefined) {
+      for (let i = 0; i < this.width; i++) {
+        result += c.repeat(this.width) + '\n';
+      }
+      console.log(result.slice(0,-1));
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = Square;
