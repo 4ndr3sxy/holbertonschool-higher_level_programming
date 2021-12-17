8c79eb42cfe44cbe9dce72feb2ba4d4846b00a5f
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-16 22:04:16 -0500

    [ADD] - Creation code and file task 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+
+import sys
+from sqlalchemy.orm import Session
+from model_city import Base, City
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    query_inner = session.query(State.name, City.id, City.name).join(
+        City, State.id == City.state_id).order_by(City.id)
+    for values in query_inner:
+        print("{}: ({}) {}".format(values[0], values[1], values[2]))
+    session.close()
