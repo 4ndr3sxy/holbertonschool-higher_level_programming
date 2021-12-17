71d9d8568fd7af25147cbcf033ac9d6aac3f05b0
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-16 22:00:50 -0500

    [ADD] - Creation code and file task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""File to connect a DB using MySQLdb"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    info = cur.fetchall()
+
+    for state in info:
+        print(state)
+
+    cur.close()
+    db.close()
