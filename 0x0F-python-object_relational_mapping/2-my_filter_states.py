c3369f2298346feeeb2fe11c753322a6c5b4110b
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-18 09:03:17 -0500

    [UPD] - query in execute

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,9 +10,8 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' \
-    ORDER BY id ASC".format(sys.argv[4])
-    cur.execute(query)
+    cur.execute("""SELECT * FROM states WHERE name = '{}'
+    ORDER BY id ASC""".format(sys.argv[4]))
 
     info = cur.fetchall()
 
