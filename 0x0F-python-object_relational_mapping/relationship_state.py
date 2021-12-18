321392baec8fba911ce97f30f0059390671ff1ac
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-18 11:39:08 -0500

    [UPD] - pep8 Style

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Model ORM to table states"""
 
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
+
 
 class State(Base):
     """ORM to table states"""
