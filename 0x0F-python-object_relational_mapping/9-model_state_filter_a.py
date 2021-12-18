041e5d05a64ddce818cd102c38c497f690986b40
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-18 11:00:34 -0500

    [UPD] - line too long in pep8

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-
-    for state in session.query(State).filter(State.name.like("%a%")).order_by(State.id).all():
+    query = (session.query(State).filter(State.name.like("%a%")).
+             order_by(State.id).all())
+    for state in query:
         print("{}: {}".format(state.id, state.name))
 
     session.close()
