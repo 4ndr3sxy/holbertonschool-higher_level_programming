1a4f1b2e5193d305642dbdf2cd0b8eb2c33b733d
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-18 10:28:25 -0500

    [UPD] - Edge case to print 'Nothing'

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,6 +7,7 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
+    flag = False
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
@@ -14,5 +15,9 @@ if __name__ == "__main__":
     session = Session(engine)
 
     for state in session.query(State).filter(State.name.like("%a%")).order_by(State.id).all():
+        flag = True
         print("{}: {}".format(state.id, state.name))
+    if flag is False:
+        print("Nothing")
+
     session.close()
