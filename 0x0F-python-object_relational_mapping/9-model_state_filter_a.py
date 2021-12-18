172c8e5f5200c6cc00ac518916e0e61fb007fb6e
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-12-18 10:46:28 -0500

    [DEL] - changes, bad commit

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
+"""lists all State objects that contain the letter a"""
 
 import sys
 from sqlalchemy.orm import Session
@@ -7,7 +7,6 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    flag = False
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
@@ -15,9 +14,6 @@ if __name__ == "__main__":
     session = Session(engine)
 
     for state in session.query(State).filter(State.name.like("%a%")).order_by(State.id).all():
-        flag = True
         print("{}: {}".format(state.id, state.name))
-    if flag is False:
-        print("Nothing")
 
     session.close()
