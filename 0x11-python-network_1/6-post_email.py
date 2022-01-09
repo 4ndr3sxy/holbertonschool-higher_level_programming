e8684ebeb9ac334a8ef44cc534c9b020957967a1
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-01-09 17:34:55 -0500

    [FIX] - Pep8 style

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-
+"""Send email using requests"""
 import requests
 import sys
 
 if __name__ == "__main__":
     payload = {'email': sys.argv[2]}
     r = requests.post(sys.argv[1], data=payload)
-    # data = urllib.parse.urlencode({"email": sys.argv[2]})
-    # data = data.encode('ascii')
-    # url = sys.argv[1]
-    # with urllib.request.urlopen(url, data) as response:
-    #     print(response)
-
-
