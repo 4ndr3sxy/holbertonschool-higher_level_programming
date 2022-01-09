ee9d290a54084eb52f59e116934759ff630e22dd
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-01-09 17:44:37 -0500

    [UPD] - flag 's' added

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cURL headers send arg in url
-curl -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST $1
+curl -d "email=test@gmail.com&subject=I will always be here for PLD" -sX POST $1
