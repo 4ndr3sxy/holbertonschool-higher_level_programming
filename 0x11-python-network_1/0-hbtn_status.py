ce61463dfdc151d5f3f88a04384dfdea46fefc3b
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-01-09 18:12:19 -0500

    [UPD] - prints inside of 'with' statement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-    print("Body response:")
-    print("\t- type: {}\n\t- content: {}".format(type(html), html))
-    print("\t- utf8 content: {}".format(html.decode('UTF-8')))
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}".format(type(html), html))
+        print("\t- utf8 content: {}".format(html.decode('UTF-8')))
