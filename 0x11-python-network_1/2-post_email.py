50530fa8123b75f585bb9e28a0c8a38d129be011
Andres Castaneda <andrescastanedaxy@gmail.com>
2022-01-08 20:12:18 -0500

    [ADD] - Creation code and file to task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({"email": sys.argv[2]})
+    data = data.encode('ascii')
+    url = sys.argv[1]
+    with urllib.request.urlopen(url, data) as response:
+        print(response)
+
+
