cce3a1b8f9b7be3b837433242afa35da7df38a12
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-01-09 18:20:11 -0500

    [UPD] - pass argument

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,7 +4,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get(sys.argv[1])
     for header, value in r.__dict__['headers'].items():
         if header == "X-Request-Id":
             print(value)
