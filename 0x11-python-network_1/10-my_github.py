c5e82e6bed89296ae6076b44ce63c8daa8a16eae
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-01-09 17:38:22 -0500

    [UPD] - Documentation and pep8 style

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""show id of an user in github"""
 import requests
 import sys
 
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
 
-    login = requests.get('https://api.github.com/user', auth=(username,token))
+    login = requests.get('https://api.github.com/user', auth=(username, token))
     print(login.json().get("id"))
