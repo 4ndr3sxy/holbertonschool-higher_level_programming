b8b3bd52a894643d990e528a10ac7e8a2b9ef739
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-01-09 20:51:55 -0500

    [FIX] - fix argument

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,9 +5,9 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    r = requests.get(argv[0])
+    r = requests.get(argv[1])
 
-    if r.status_code < 400:
+    if 400 > r.status_code:
         print(r.text)
     else:
         print("Error code: {}".format(r.status_code))
