e1d08dae0a78e717a30fefb1cf621af663763dd9
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-01 13:14:58 -0500

    [UPD] - managment variable err

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,7 @@
 
 const args = process.argv;
 const request = require('request');
-request(args[2], function (response) {
+request(args[2], function (err, response) {
+  if (err) throw err;
   console.log('code:', response && response.statusCode); // Print the response status code if a response was received
 });
