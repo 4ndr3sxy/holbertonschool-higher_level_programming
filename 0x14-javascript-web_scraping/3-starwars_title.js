bcbb242f08c7d898306c2e798ce20c4c6086f738
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-01 13:13:00 -0500

    [UPD] - semistandar style

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,6 +3,7 @@
 const args = process.argv;
 const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/' + args[2], function (err, request) {
+  if (err) throw err;
   const json = JSON.parse(request.body);
   console.log(json.title); // Print the HTML for the Google homepage.
 });
